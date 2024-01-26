c0d3ad7c7824ef2bc674e6d5d8770dab3cabc896
HolySkyMin <fsda109238@gmail.com>
2024-01-26 23:20:49 +0900

    게임 씬 UI 작업 중

---
+++ Assets/Scripts/UIRequirement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRequirement : MonoBehaviour
+{
+    // TODO: Requirement 객체 가지기
+    [SerializeField] private Image backImage;
+    [SerializeField] private Text textLabel;
+    [SerializeField] private GameObject completedCheck;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
