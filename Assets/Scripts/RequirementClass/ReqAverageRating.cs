80c77cce87bd081f639b6324f5ead91595c18644
Noperi0r <mathewgwon@gmail.com>
2024-01-27 05:29:46 +0900

    ScheduleFactory Complete, Requirement Implementation

--- Assets/Scripts/RequirementClass/ReqAverageRating.cs
+++ Assets/Scripts/RequirementClass/ReqAverageRating.cs
@@ -11,26 +11,20 @@ public class ReqAverageRating : RequirementBase
     {
         base.GetLogicManager();
 
-        reqAverageRating = Random.Range(4, 9) / 2f;
+        reqAverageRating = Random.Range(4, 9) / 2f; // 2,2.5, 3, 3.5, 4 중 하나
 
         contentTitle = string.Format("교수 평균 평점 {0}점 이상", reqAverageRating);
     }
 
     public override bool DoesMeetRequirement()
     {
-        // ★★Implement: GameManager 업데이트 후 currentStudent 받아오는거 변경 필수 안 그럼 에러 발생 
-        Student currentStudent = new Student();
-        gLogicManager.GetCurrentStudent(currentStudent);
-
-        List<Lecture> finalLectures = new List<Lecture>();
-        currentStudent.GetCurrentLectures(finalLectures);
-
         int finalLecCount = 0;
         float finalAverageRating = 0;
-        foreach(Lecture lecture in finalLectures)
+        
+        foreach(Lecture lecture in gLogicManager.GetSelectedLectures())
         {
             ++finalLecCount;
-            finalAverageRating += lecture.profRating;
+            finalAverageRating += lecture.Rating;
         }
 
         finalAverageRating /= finalLecCount;
---
+++ Assets/Scripts/ScheduleFactory.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public static class ScheduleFactory
+{
+    static bool TFRandomDecide()
+    {
+        return (Random.Range(0, 2) == 1 ? true : false);
+    }
+
+    // int값 따라서 생성 1학점이면 1개, 2학점이면 2개, 3학점이면 3개 셀 
+    static List<Vector2Int> GenerateLectureBlock(int credit)
+    {
+        List<Vector2Int> returnBlock = new List<Vector2Int>();
+
+        // x col(요일) 0~4 / y row(시간) 0~7
+        // 월요일x 1교시y
+        int xPos = 0, yPos = 0;
+        int xMin = 0, xMax = 5;
+        int yMin = 0, yMax = 8;
+
+        switch(credit)
+        {
+            case 1:
+                returnBlock.Add(new Vector2Int(Random.Range(xMin, xMax), Random.Range(yMin, yMax)));
+                break;
+
+            case 2:
+                if(TFRandomDecide()) // 오른쪽으로 블록 추가하는 경우
+                {
+                    xPos = Random.Range(xMin, xMax-1); // 0 1 2 3 
+                    yPos = Random.Range(yMin, yMax);
+                    returnBlock.Add(new Vector2Int(xPos, yPos));
+                    returnBlock.Add(new Vector2Int(xPos + 1, yPos));
+                }
+                else // 아래쪽으로 블록 추가하는 경우
+                {
+                    xPos = Random.Range(xMin, xMax);
+                    yPos = Random.Range(yMin, yMax - 1);
+                    returnBlock.Add(new Vector2Int(xPos, yPos));
+                    returnBlock.Add(new Vector2Int(xPos, yPos+1));
+                }
+                break;
+            case 3:
+                if(TFRandomDecide()) // true: Line형
+                {
+                    if(TFRandomDecide()) // true: 오른쪽 전개
+                    {
+                        xPos = Random.Range(xMin, xMax - 2);
+                        yPos = Random.Range(yMin, yMax);
+                        returnBlock.Add(new Vector2Int(xPos, yPos));
+                        returnBlock.Add(new Vector2Int(xPos+1, yPos));
+                        returnBlock.Add(new Vector2Int(xPos+2, yPos));
+                    }
+                    else // false: 아래쪽 전개
+                    {
+                        xPos = Random.Range(xMin, xMax);
+                        yPos = Random.Range(yMin, yMax - 2);
+                        returnBlock.Add(new Vector2Int(xPos, yPos));
+                        returnBlock.Add(new Vector2Int(xPos, yPos+1));
+                        returnBlock.Add(new Vector2Int(xPos, yPos+2));
+                    }
+                }
+                else // ㄴ자형 
+                {
+                    // Set x
+                    bool generateLeft = TFRandomDecide();
+                    if(generateLeft) // true: 왼쪽 생성
+                    {
+                        xPos = Random.Range(xMin+1, xMax);
+                    }
+                    else // false: 오른쪽 생성
+                    {
+                        xPos = Random.Range(xMin, xMax-1);
+                    }
+
+                    // Set y
+                    bool generateUp = TFRandomDecide();
+                    if(generateUp) // true: 위쪽 생성
+                    {
+                        yPos = Random.Range(yMin + 1, yMax);
+                    }
+                    else // 아래쪽 생성
+                    {
+                        yPos = Random.Range(yMin, yMax-1);
+                    }
+
+                    returnBlock.Add(new Vector2Int(xMin, yPos));
+                    
+                    // 좌 우 블록 중 하나 생성
+                    if(generateLeft)
+                    {
+                        returnBlock.Add(new Vector2Int(xPos-1, yPos));
+                    }
+                    else
+                    {
+                        returnBlock.Add(new Vector2Int(xPos+1, yPos));
+                    }
+
+                    // 위 아래 블록 중 하나 생성
+                    if(generateUp)
+                    {
+                        returnBlock.Add(new Vector2Int(xPos, yPos-1));
+                    }
+                    else
+                    {
+                        returnBlock.Add(new Vector2Int(xPos, yPos+1));
+                    }
+                }
+                break;
+            default:
+                Debug.Log("Wrong credit input in ScheduleFactory");
+                break;
+        }
+
+        return returnBlock;
+    }
+    
+    // output list<vector2int>
+
+}
